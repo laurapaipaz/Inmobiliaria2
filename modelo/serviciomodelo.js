1bbb70eb7f5bc1e609ba8dfe737b2205ec4c3fb4
laurapaipaz <laurapaipaz@gmail.com>
2021-11-15 23:41:57 -0500

    Hecho

--- modelo/serviciomodelo.js
+++ modelo/serviciomodelo.js
@@ -1,27 +1,57 @@
-var modeloUsario = require('./usuario')
-var modeloubicacione = require('./ubicacione')
-var modeloinmueble = require('./ubicacione')
+const express=require("express")
+const {createReadStream}=require('fs')
+//var modeloUsario = require('./usuario')
+//var modeloubicacione = require('./ubicacione')
+//var modeloinmueble = require('./ubicacione')
 
+const app=new express();
+const HTML_CONTENT_TYPE = 'text/html'
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+const path = require("path");
+var modeloUsario= require('./usuario')
 
 require('./conexion')
 //var modelo2=require('./zonas')
-//app.post("/", (req, res) => {
 
-var myobj = { cedula: "101474587", nombre: "Santiago", apellido: "Colorado", correo: "santi@gmail.com", telefono: "3214141479", clave: "12245347"};
-modeloUsario.collection.insertOne(myobj, function (err, res) {
+app.post("/upload", (req, res) => {
+    var myobj = { cedula: req.body.cedula, nombre:req.body.nombre, apellido:req.body.apellido, correo: req.body.correo,telefono: req.body.telefono, clave: req.body.clave  };
+    modelo.collection.insertOne(myobj, function(err, res) {
     if (err) throw err;
-    console.log("1 documento insertado");
-})
-
-var myobj = { ciudad: "Villavicencio", zona: "Sur", barrio: "Florida", direccion: "calle 45C-12"};
-modeloubicacione.collection.insertOne(myobj, function (err, res) {
-    if (err) throw err;
-    console.log("2 documento insertado");
-})
-
-var myobj = { tipo: "Apartamento", no_hab: "4", ubicacion: "618dc18b8fdeba31bfe9bb24", precio: 20000, telefono: "12457", imagen: "ruta/img.png" };
-modeloinmueble.collection.insertOne(myobj, function (err, res) {
-    if (err) throw err;
-    console.log("3 documento insertado");
-})
-//})
+  
+    })
+    res.send("datos creados")
+    })
+  
+  
+  
+  
+  
+  /*
+  modeloUsario.find({}, (err, docs) => {
+  
+       console.log(docs[2].nombre)
+  
+  })
+  */
+    //})
+  
+    app.get('/', (req, res) => {
+      res.writeHead(200, { 'Content-Type': HTML_CONTENT_TYPE })
+   
+  
+    createReadStream('./index.html').pipe(res)
+  //res.end("hola mundo");
+     
+    })
+  
+  
+    app.listen(600, () => {
+  
+      console.log("aplicacion corriendo en el puerto 600")
+      
+      })
---
+++ public/main.js
@@ -0,0 +1,39 @@
+var ngApp = angular.module('myApp', []);
+  
+ ngApp.controller('myController', function ($scope, $http) {
+
+    $scope.msg = '¡Bienvenido! ';
+
+    $scope.getData = function () {
+                   return 'qubernet';
+                }
+                $scope.createTodo = function () {
+                    $http
+                    .post("http://localhost:600/upload", $scope.formData)
+                    .then(function successCallback(response)  {
+                     // $scope.formData = {};
+                      $scope.todos = response.data;
+                      console.log(response);
+                    })
+                   
+                //   alert($scope.tel+" "+$scope.cedula)
+                  };
+ })
+
+
+ onload = function () {
+             document.getElementById('lbtnTest').onclick = function () {
+                 alert("En este espacio puedes crear tu usuario")
+      // Obtenga la aplicación Angular a través del controlador
+                 var appElement = document.querySelector('[ng-controller=myController]');
+      // Obtenga la variable $ scope
+                var $scope = angular.element(appElement).scope();
+                 
+      // Llama a la variable msg y cambia el valor de msg
+                 $scope.msg = '*Todos los campos son requeridos';
+      // La línea anterior cambió el valor de msg. Si desea sincronizar con el controlador angular, debe llamar al método $ apply ().
+                 $scope.$apply();
+      // Llame al método getData () en el controlador
+             console.log($scope.getData());
+              }
+        }
